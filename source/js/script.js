3cf4776a6e04f62658a913887ff06ddc43b16d8d
Anna Ignatova <uventa2@yandex.ru>
2023-04-26 17:55:37 +0300

    Добавляет правки

--- source/js/script.js
+++ source/js/script.js
@@ -2,13 +2,13 @@ let pageBody = document.querySelector('.page__body');
 let navMain = document.querySelector('.navigation');
 let navToggle = document.querySelector('.navigation__toggle');
 
-pageBody.classList.remove('navigation--nojs');
+pageBody.classList.remove('nojs');
 navMain .classList.remove('navigation--opened');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('navigation--opened')) {
-    navMain.classList.remove('navigation--opened');
+    navMain.classList.remove('navigation--opened')
   } else {
-    navMain.classList.add('navigation--opened');
+    navMain.classList.add('navigation--opened')
   }
 });
